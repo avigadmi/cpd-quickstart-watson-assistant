c1d111546b8baef23811a2e5a51bc87c126d46bd
Avigad <avigadm@ibm.com>
2019-08-13 14:24:35 +0300

    logs

--- server/service.js
+++ server/service.js
@@ -1,11 +1,11 @@
 const AssistantV1 = require('watson-developer-cloud/assistant/v1');
 
 const assistant = new AssistantV1({
-  iam_apikey: process.env.ASSISTANT_IAM_APIKEY,
-  username: process.env.ASSISTANT_USERNAME,
-  password: process.env.ASSISTANT_PASSWORD,
-  url: process.env.ASSISTANT_URL,
-  version: '2018-07-10',
+    iam_apikey: process.env.ASSISTANT_IAM_APIKEY,
+    username: process.env.ASSISTANT_USERNAME,
+    password: process.env.ASSISTANT_PASSWORD,
+    url: process.env.ASSISTANT_URL,
+    version: '2018-07-10',
 });
 
 let workspaceId = null;
@@ -18,41 +18,41 @@ const MAX_RETRIES = 3;
  * @param {Object} trainingFile Config file used to create workspace
  */
 const createWorkspace = (assistant, retryCount, trainingFile) => {
-  assistant.createWorkspace(trainingFile, function(err, response) {
-    if (err) {
-      if (retryCount > 1) {
-        --retryCount;
-        createWorkspace(assistant, retryCount, trainingFile);
-      } else {
-        console.log('Can not create workspace! Please go to Launch Tool provided in starter kits app page to create workspace manually');
-      }
-    } else {
-      workspaceId = response.workspace_id;
-      console.log(`Workspace has been created (${workspaceId})`);
-    }
-  });
+    assistant.createWorkspace(trainingFile, function (err, response) {
+        if (err) {
+            if (retryCount > 1) {
+                --retryCount;
+                createWorkspace(assistant, retryCount, trainingFile);
+            } else {
+                console.log('Can not create workspace! Please go to Launch Tool provided in starter kits app page to create workspace manually');
+            }
+        } else {
+            workspaceId = response.workspace_id;
+            console.log(`Workspace has been created (${workspaceId})`);
+        }
+    });
 };
 
 
 /**
  * Creates a workspace or use an existing one
-*/
-assistant.listWorkspaces(function(err, response) {
-  if (err) {
-    console.log(err);
-    return;
-  } else if (response.workspaces.length > 0) {
-    workspaceId = response.workspaces[0].workspace_id;
-    console.log('Using workspace:', workspaceId);
-  } else {
-    console.log('Creating a workspace...');
-    createWorkspace(assistant, MAX_RETRIES, require('../training/bank_simple_workspace.json'));
-  }
+ */
+assistant.listWorkspaces(function (err, response) {
+    console.log("listing workspaces...");
+    if (err) {
+        console.log(err);
+        return;
+    } else if (response.workspaces.length > 0) {
+        workspaceId = response.workspaces[0].workspace_id;
+        console.log('Using workspace:', workspaceId);
+    } else {
+        console.log('Creating a workspace...');
+        createWorkspace(assistant, MAX_RETRIES, require('../training/bank_simple_workspace.json'));
+    }
 });
 
 
-
 module.exports = {
-  getAssistantV1: () => assistant,
-  getWorkspaceId: () => workspaceId,
+    getAssistantV1: () => assistant,
+    getWorkspaceId: () => workspaceId,
 }
